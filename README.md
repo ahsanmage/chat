c4889ee9e7c519e09ccd2e30298ca4461c9f0973
ahsandev <ahsan@bysymphony.com>
2017-08-29 12:25:26 +0400

    Base Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+Installation:
+
+Running on local
+Make sure you have Node.js and npm installed.
+
+Clone git repository
+
+$ git clone https://github.com/ahsanmage/chat.git
+$ cd chat/
+
+Install Dependencies
+$ npm install
+Edit configuration file in app/config/config.json with your mongodb credentials.
+You need to create a database on MongoLab, then create a database user
+
+Start the application
+
+$ npm start
+
+Your app should now be running on localhost:3001
---
+++ app/auth/index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var config 		= require('../config');
+var passport 	= require('passport');
+var logger 		= require('../logger');
+
+var LocalStrategy 		= require('passport-local').Strategy;
+
+var User = require('../models/user');
+
+/**
+ * Encapsulates all code for authentication 
+ * Either by using username and password, or by using social accounts
+ *
+ */
+var init = function(){
+
+	// Serialize and Deserialize user instances to and from the session.
+	passport.serializeUser(function(user, done) {
+		done(null, user.id);
+	});
+
+	passport.deserializeUser(function(id, done) {
+		User.findById(id, function (err, user) {
+			done(err, user);
+		});
+	});
+
+	// Plug-in Local Strategy
+	passport.use(new LocalStrategy(
+	  function(username, password, done) {
+	    User.findOne({ username: new RegExp(username, 'i'), socialId: null }, function(err, user) {
+	      if (err) { return done(err); }
+
+	      if (!user) {
+	        return done(null, false, { message: 'Incorrect username or password.' });
+	      }
+
+	      user.validatePassword(password, function(err, isMatch) {
+	        	if (err) { return done(err); }
+	        	if (!isMatch){
+	        		return done(null, false, { message: 'Incorrect username or password.' });
+	        	}
+	        	return done(null, user);
+	      });
+
+	    });
+	  }
+	));
+	return passport;
+}
+	
+module.exports = init();
